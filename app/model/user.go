f1d2d7d980cdf89bc1559a804a4459c7cd7b85ef
satriawildan45 <120617391+satriawildan45@users.noreply.github.com>
2025-12-16 12:17:40 +0700

    Update

--- app/model/user.go
+++ app/model/user.go
@@ -3,15 +3,16 @@ package models
 import "time"
 
 type User struct {
-	ID           string    `db:"id" json:"id"`
-	Username     string    `db:"username" json:"username"`
-	Email        string    `db:"email" json:"email"`
-	PasswordHash string    `db:"password_hash" json:"-"`
-	FullName     string    `db:"full_name" json:"full_name"`
-	RoleID       string    `db:"role_id" json:"role_id"`
-	IsActive     bool      `db:"is_active" json:"is_active"`
-	CreatedAt    time.Time `db:"created_at" json:"created_at"`
-	UpdatedAt    time.Time `db:"updated_at" json:"updated_at"`
+	ID           string     `json:"id"`
+	Username     string     `json:"username"`
+	Email        string     `json:"email"`
+	PasswordHash string     `json:"-"`
+	FullName     string     `json:"full_name"`
+	RoleID       string     `json:"role_id"`
+	IsActive     bool       `json:"is_active"`
+	DeletedAt    *time.Time `json:"deleted_at,omitempty"`
+	CreatedAt    time.Time  `json:"created_at"`
+	UpdatedAt    time.Time  `json:"updated_at"`
 }
 
 type LoginRequest struct {
