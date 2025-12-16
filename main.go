cc41006d2e5634db101c72bcc52587a715050973
satriawildan45 <120617391+satriawildan45@users.noreply.github.com>
2025-12-16 12:15:43 +0700

    Memperbaiki Code

--- main.go
+++ main.go
@@ -1,18 +1,41 @@
+// @title Alumni Management System API
+// @version 1.0
+// @description API untuk sistem manajemen alumni dengan fitur RBAC dan achievement management
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name MIT
+// @license.url https://opensource.org/licenses/MIT
+
+// @host localhost:3000
+// @BasePath /api/v1
+
+// @securityDefinitions.apikey BearerAuth
+// @in header
+// @name Authorization
+// @description Type "Bearer" followed by a space and JWT token.
+
 package main
 
 import (
+	"crud-app/app/utils"
 	"crud-app/database"
 	"crud-app/route"
-	"fmt"
 	"log"
 	"os"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
-	"golang.org/x/crypto/bcrypt"
+
+	_ "crud-app/app/docs" // Import generated docs
+
+	fiberSwagger "github.com/swaggo/fiber-swagger"
 )
 
 func main() {
-	// 🔹 Load .env file
 	err := godotenv.Load()
 	if err != nil {
 		log.Println("No .env file found, using system environment variables")
@@ -25,24 +48,26 @@ func main() {
 	database.ConnectDB()
 	defer database.DB.Close()
 
+	mongoClient := database.MongoConnection()
+	defer database.CloseDB(mongoClient)
+	mongoDB := database.GetMongoDatabase()
+
+	utils.InitCache()
+	log.Println("Permission cache initialized")
+
 	app := fiber.New()
 
-	// Contoh hash password
-	password := "123456"
-	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Hash:", string(hash))
+	app.Get("/swagger/*", fiberSwagger.WrapHandler)
 
-	// Register routes
-	route.RegisterRoutes(app, database.DB)
+	route.Routes(app, database.DB, mongoDB)
 
-	// 🔹 Pakai APP_PORT dari .env
 	port := os.Getenv("APP_PORT")
 	if port == "" {
 		port = "3000"
 	}
+
+	log.Printf("Server running on http://localhost:%s", port)
+	log.Printf("Swagger docs: http://localhost:%s/swagger/", port)
 
 	log.Fatal(app.Listen(":" + port))
 }
